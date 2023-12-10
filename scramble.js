7aaed570c6199776dfcaf46a48f619240e7c9f79
Sosna123 <olek.arez@gmail.com>
2023-12-10 16:02:29 +0100

    typescript (some errors to solve)

--- scramble.js
+++ scramble.js
@@ -1,90 +1,74 @@
-function getMovesArr(type='3x3'){
+"use strict";
+function getMovesArr(type = '3x3') {
     let possibleMoves, moves, moveCount;
-
-    //* check if type is a string
-    type = typeof type === 'string' ? type : '3x3';
-
-    if(type === '3x3'){
+    if (type === '3x3') {
         //* 3x3
         moves = ['R', 'L', 'B', 'F', 'U', 'D'];
         possibleMoves = moves.length;
         moveCount = 20;
-    }else if(type === '2x2'){
+    }
+    else if (type === '2x2') {
         //* 2x2
         moves = ['R', 'F', 'U'];
         possibleMoves = moves.length;
         moveCount = 10;
-    }else if(type === '4x4'){
+    }
+    else if (type === '4x4') {
         //* 4x4
         moves = ['R', 'Rw', 'L', 'Lw', 'B', 'Bw', 'F', 'Fw', 'U', 'Uw', 'D', 'Dw'];
         possibleMoves = moves.length;
         moveCount = 45;
-    }else if(type === 'skweb'){
+    }
+    else if (type === 'skweb') {
         //* skweb
         moves = ['R', 'L', 'U', 'B'];
         possibleMoves = moves.length;
         moveCount = 9;
-    }else{
+    }
+    else {
         //* 3x3 if input is wrong
         moves = ['R', 'L', 'B', 'F', 'U', 'D'];
         possibleMoves = moves.length;
         moveCount = 20;
     }
-
-    return [moves, possibleMoves, moveCount]
+    return { moves, possibleMoves, moveCount };
 }
-
-function chooseMove(old1, type, moves, possibleMoves, moveCount){
-    type = typeof type === 'string' ? type : '3x3';
-    moves = typeof moves === 'object' ? moves: ['R', 'L', 'B', 'F', 'U', 'D'];
-    moveCount = typeof moveCount === 'number' ? moveCount: 20;
-    old1 = typeof old1 === undefined ? 'no last move' : old1;
-
-
+function chooseMove(old1, type, moves, possibleMoves, moveCount) {
     //* check which type of move
-    let id1 = Math.floor(Math.random() * possibleMoves)
+    let id1 = Math.floor(Math.random() * possibleMoves);
     id1 = id1 === possibleMoves ? id1-- : id1;
-    
-    
     //* check if moves are the same, as last time
-    if(typeof old1 !== 'undefined' && old1.toString() === id1.toString()){
-        let [moveNew, id1New, moveCountNew] = chooseMove(old1, type, moves, possibleMoves, moveCount)
+    if (old1.toString() === id1.toString()) {
+        let [moveNew, id1New, moveCountNew] = chooseMove(old1, type, moves, possibleMoves, moveCount);
         return [moveNew, id1New, moveCountNew];
     }
     //* change the second symbol of the move
-    const id2 = Math.round(Math.random() * 2)
-    let move = moves[id1]
-    if(id2 === 1){
-        move += "'"
+    const id2 = Math.round(Math.random() * 2);
+    let move = moves[id1];
+    if (id2 === 1) {
+        move += "'";
     }
-    if(id2 === 2){
-        move += "2"
+    if (id2 === 2) {
+        move += "2";
     }
-
-    return [move, id1]
+    return [move, id1];
 }
-
-function scramble(type){
-    type = typeof type === 'string' ? type : '3x3';
-
+function scramble(type) {
     //* make movecount work
-    let [moves, possibleMoves, moveCount] = getMovesArr(type.toString());
-
+    let moves = getMovesArr(type.toString()).moves;
+    let possibleMoves = getMovesArr(type.toString()).possibleMoves;
+    let moveCount = getMovesArr(type.toString()).moveCount;
     //* some values
-    const p = document.querySelector('p')
     const moveCountRandom = Math.trunc(Math.random() * 2) + (moveCount - 1);
     const scramble = [];
-    let old1;
-
+    let old1 = '';
     //* invoke chooseMove function a lot of times
-    for(let i = 0; i < moveCountRandom; i++){
-        const [move, id1] = chooseMove(old1, type, moves, possibleMoves, moveCount)
+    for (let i = 0; i < moveCountRandom; i++) {
+        const [move, id1] = chooseMove(old1, type, moves, possibleMoves, moveCount);
         old1 = id1;
         scramble.push(move);
     }
-    scrambleText = scramble.join(' ')
-
+    let scrambleText = scramble.join(' ');
     //* return scramble
-    // p.innerText = `Scramble: ${scrambleText}`
-    return scrambleText
+    return scrambleText;
 }
---
+++ scramble.ts
@@ -0,0 +1,87 @@
+type getMovesArrType = {
+    moves: string[],
+    possibleMoves: number,
+    moveCount: number
+}
+
+function getMovesArr(type: string='3x3'): getMovesArrType{
+    let possibleMoves: number, moves: string[], moveCount: number;
+
+    if(type === '3x3'){
+        //* 3x3
+        moves = ['R', 'L', 'B', 'F', 'U', 'D'];
+        possibleMoves = moves.length;
+        moveCount = 20;
+    }else if(type === '2x2'){
+        //* 2x2
+        moves = ['R', 'F', 'U'];
+        possibleMoves = moves.length;
+        moveCount = 10;
+    }else if(type === '4x4'){
+        //* 4x4
+        moves = ['R', 'Rw', 'L', 'Lw', 'B', 'Bw', 'F', 'Fw', 'U', 'Uw', 'D', 'Dw'];
+        possibleMoves = moves.length;
+        moveCount = 45;
+    }else if(type === 'skweb'){
+        //* skweb
+        moves = ['R', 'L', 'U', 'B'];
+        possibleMoves = moves.length;
+        moveCount = 9;
+    }else{
+        //* 3x3 if input is wrong
+        moves = ['R', 'L', 'B', 'F', 'U', 'D'];
+        possibleMoves = moves.length;
+        moveCount = 20;
+    }
+
+    return {moves, possibleMoves, moveCount}
+}
+
+function chooseMove(old1: number|string, type: string, moves: string[], possibleMoves: number, moveCount: number): [string|number]{
+    //* check which type of move
+    let id1: number = Math.floor(Math.random() * possibleMoves)
+    id1 = id1 === possibleMoves ? id1-- : id1;
+    
+    
+    //* check if moves are the same, as last time
+    if(old1.toString() === id1.toString()){
+        let [moveNew, id1New, moveCountNew] = chooseMove(old1, type, moves, possibleMoves, moveCount)
+        return [moveNew, id1New, moveCountNew];
+    }
+    //* change the second symbol of the move
+    const id2: number = Math.round(Math.random() * 2)
+    let move: string = moves[id1]
+
+    if(id2 === 1){
+        move += "'"
+    }
+    if(id2 === 2){
+        move += "2"
+    }
+
+    return [move, id1]
+}
+
+function scramble(type: string): string{
+    //* make movecount work
+    let moves = getMovesArr(type.toString()).moves;
+    let possibleMoves = getMovesArr(type.toString()).possibleMoves;
+    let moveCount = getMovesArr(type.toString()).moveCount;
+
+    //* some values
+    const moveCountRandom: number = Math.trunc(Math.random() * 2) + (moveCount - 1);
+    const scramble: string[] = [];
+    let old1: number|string = '';
+
+    //* invoke chooseMove function a lot of times
+    for(let i = 0; i < moveCountRandom; i++){
+        const [move, id1] = chooseMove(old1, type, moves, possibleMoves, moveCount)
+        old1 = id1;
+        scramble.push(move);
+    }
+    
+    let scrambleText: string = scramble.join(' ')
+
+    //* return scramble
+    return scrambleText
+}
